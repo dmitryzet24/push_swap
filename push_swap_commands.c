20ad507e48d8e9e15d3fc7482cc9c0cdce781419
Daniil Sutormin <dsutormi@c1r6s8.42porto.com>
2026-05-13 20:12:00 +0100

    added disorder check for stack b

--- push_swap_commands.c
+++ push_swap_commands.c
@@ -67,9 +67,10 @@ void	ft_execute(char *c, t_stack **a, t_stack **b, t_command *comms)
 		{
 			comms[i].func(a, b);
 			printf("stack a\n");
-			printf("disorder value %d\n", ft_compute_disorder(*a));
+			printf("disorder value [%d]\n", ft_compute_disorder(*a));
 			ft_print_stack(*a);
 			printf("stack b\n");
+			printf("disorder value [%d]\n", ft_compute_disorder(*b));
 			ft_print_stack(*b);
 			return ;
 		}
